11fbe396b8a1b422e09086f893e4d13d9994440d
Yoga Lin <blueandhack@gmail.com>
2017-02-04 02:13:15 -0700

    Update wordify

--- cs372p1/wordify.rb
+++ cs372p1/wordify.rb
@@ -1,8 +1,21 @@
 save_filename= ARGV[1]
 target_filename= ARGV[3]
-puts target_filename
-puts save_filename
+
 file = File.new(target_filename, 'r')
+str = ''
 while (line = file.gets)
-  puts line
+  line.chomp!
+  line.each_char do |index|
+    if (index >= 'a' && index <='z') || (index >= 'A' && index <='Z') || (index == '-') || (index>='0' &&index<='9')
+      str += index
+    else
+      str += ' '
+    end
+  end
+end
+
+words = str.split(' ')
+
+File.open(save_filename, 'w+') do |f|
+  f.puts(words)
 end
