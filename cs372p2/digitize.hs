ed6eed0c9890c28b275691a889fc9420a2dc225c
Yoga Lin <blueandhack@gmail.com>
2017-03-06 10:16:32 -0700

    Updated digitize.hs

--- cs372p2/digitize.hs
+++ cs372p2/digitize.hs
@@ -1,10 +1,43 @@
 module Main where
 
-
 digitize :: String -> String
-digitize a = a
+digitize a = do
+    let numbers = digitizeHelper a
+    let lineOne = getString numbers 0 ++ "\n"
+    let lineTwo = getString numbers 1 ++ "\n"
+    let lineThree = getString numbers 2 ++ "\n"
+    lineOne ++ lineTwo ++ lineThree
+
+getString :: [[String]] -> Int -> String
+getString s a
+    | null s = []
+    | otherwise = head s!!a ++ getString (tail s) a
+
+digitizeHelper :: String -> [[String]]
+digitizeHelper s
+    | null s = []
+    | otherwise = do
+        let number = getOneNumber (head s)
+        number : digitizeHelper (tail s)
+
+getOneNumber :: Char -> [String]
+getOneNumber s
+    | s == '0' = [" _ ","| |","|_|"]
+    | s == '1' = ["   ","  |","  |"]
+    | s == '2' = [" _ "," _|","|_ "]
+    | s == '3' = [" _ "," _|"," _|"]
+    | s == '4' = ["   ","|_|","  |"]
+    | s == '5' = [" _ ","|_ "," _|"]
+    | s == '6' = [" _ ","|_ ","|_|"]
+    | s == '7' = [" _ ","  |","  |"]
+    | s == '8' = [" _ ","|_|","|_|"]
+    | s == '9' = [" _ ","|_|"," _|"]
+    | s == ':' = [" ",".","."]
+    | otherwise = ["","",""]
+
 
 main :: IO ()
 main = do
     putStr $ digitize "12:56"
     putStr $ digitize "10:20"
+    putStr $ digitize ""
