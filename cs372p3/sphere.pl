9539ff7a0f1c9c88c0e1ad405523be4be45566a9
Yoga Lin <blueandhack@gmail.com>
2017-04-16 02:46:32 -0700

    Updated sphere.pl

---
+++ cs372p3/sphere.pl
@@ -0,0 +1,8 @@
+% Name: Yujia Lin
+% Surface area and volume of sheres
+
+spherearea(Radius, Area) :-
+    Area is 4.0 * pi * Radius * Radius.
+spherevolume(Radius, Volume) :-
+    spherearea(Radius, Area),
+    Volume is 1/3 * Radius * Area.
