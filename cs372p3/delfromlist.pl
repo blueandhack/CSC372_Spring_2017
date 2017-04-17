c081a4acdd673b0da92bc3d4a0280899f78b9b9f
Yoga Lin <blueandhack@gmail.com>
2017-04-17 12:39:20 -0700

    Updated Program 3

--- cs372p3/delfromlist.pl
+++ cs372p3/delfromlist.pl
@@ -1,1 +1,11 @@
 % Name: Yujia Lin
+
+delfromlist(_, [], []).
+
+delfromlist(Element, [Element|List], Result) :-
+    delfromlist(Element, List, Result).
+
+delfromlist(Element, [X|List], [X|Result]) :-
+    X \== Element,
+    delfromlist(Element, List, Result),
+    X \== Element.
--- cs372p3/subsetofall.pl
+++ cs372p3/subsetofall.pl
@@ -1,1 +1,23 @@
 % Name: Yujia Lin
+memberhelper(Subset, List) :-
+    Subset == [],
+    List == [],
+    !.
+
+memberhelper([], _).
+
+memberhelper([X|Subset], List) :-
+  member(X, List),
+  memberhelper(Subset, List).
+
+
+subsettofall(Subset, List) :-
+    Subset == [],
+    List == [],
+    !.
+
+subsettofall(_, []).
+
+subsettofall(Subset, [X|List]) :-
+  memberhelper(Subset, X),
+  subsettofall(Subset, List).
