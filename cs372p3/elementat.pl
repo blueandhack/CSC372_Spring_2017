ce54baf6f6e475ba651b717a22329ed084a01ab9
Yoga Lin <blueandhack@gmail.com>
2017-04-17 07:56:55 -0700

    Updated elementat.pl

--- cs372p3/elementat.pl
+++ cs372p3/elementat.pl
@@ -1,1 +1,12 @@
 % Name: Yujia Lin
+
+elementat(Index, List, Element) :-
+    elementathelper(Index, List, Element, 0).
+
+elementathelper(Index, [X|_], Element, I) :-
+    Index == I,
+    Element = X.
+
+elementathelper(Index, [_|List], Element, I) :-
+    B is I + 1,
+    elementathelper(Index, List, Element, B).
