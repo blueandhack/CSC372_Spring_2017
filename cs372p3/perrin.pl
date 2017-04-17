46256045181d9a53db1843784a04f269bcf36b09
Yoga Lin <blueandhack@gmail.com>
2017-04-17 01:01:23 -0700

    Updated perrin.pl

--- cs372p3/perrin.pl
+++ cs372p3/perrin.pl
@@ -1,13 +1,15 @@
 % Name: Yujia Lin
 
-perrrin(1, 0).
+perrin(0, 3).
 
-perrrin(2, 2).
+perrin(1, 0).
 
-perrrin(Seqnum, Seqvalue) :-
+perrin(2, 2).
+
+perrin(Seqnum, Seqvalue) :-
     Seqnum > 2,
     S1 is Seqnum - 2,
     S2 is Seqnum - 3,
-    perrrin(S1, Se1),
-    perrrin(S2, Se2),
+    perrin(S1, Se1),
+    perrin(S2, Se2),
     Seqvalue is Se1 + Se2.
