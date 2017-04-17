e9e300fbe80eaf8212aa960404393bbf6f04769d
Yoga Lin <blueandhack@gmail.com>
2017-04-17 07:34:37 -0700

    Updated printlist.pl

--- cs372p3/printlist.pl
+++ cs372p3/printlist.pl
@@ -1,1 +1,30 @@
 % Name: Yujia Lin
+
+print135([]).
+
+print135(List) :-
+    printhelper(List, 1).
+
+printhelper([], _).
+
+printhelper([X|List], Index) :-
+    Index == 1,
+    write(X),nl,
+    I is Index + 1,
+    printhelper(List, I).
+
+printhelper([X|List], Index) :-
+    Index == 3,
+    write(X),nl,
+    I is Index + 1,
+    printhelper(List, I).
+
+printhelper([X|List], Index) :-
+    Index == 5,
+    write(X),nl,
+    I is 0,
+    printhelper(List, I).
+
+printhelper([_|List], Index) :-
+    I is Index + 1,
+    printhelper(List, I).
