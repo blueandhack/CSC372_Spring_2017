ed1b54c220fc77fbc73b8b9af3e2b7132c73e61d
Yoga Lin <blueandhack@gmail.com>
2017-04-16 20:46:08 -0700

    Updated intdiv.pl

--- cs372p3/intdiv.pl
+++ cs372p3/intdiv.pl
@@ -1,1 +1,14 @@
 % Name: Yujia Lin
+
+intdiv(_, 0, _) :-
+    write('ERROR: intdiv/3: Arithmetic: evaluation error: ‘zero_divisor’').
+
+intdiv(Dividend, Divisor, Quotient) :-
+    Dividend < Divisor,
+    Quotient is 0.
+
+intdiv(Dividend, Divisor, Quotient) :-
+    Dividend >= Divisor,
+    D is Dividend - Divisor,
+    intdiv(D, Divisor, Q),
+    Quotient is Q + 1.
