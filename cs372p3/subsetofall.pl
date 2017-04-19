35dfcbe17b336bb2688081244344d0d0c00b60d5
Yoga Lin <blueandhack@gmail.com>
2017-04-19 10:39:16 -0700

    Updated subsetofall.pl

--- cs372p3/subsetofall.pl
+++ cs372p3/subsetofall.pl
@@ -1,11 +1,22 @@
 % Name: Yujia Lin
+
+% two
+memberhelpertwo([], []).
+
+memberhelpertwo([X|Subset], List) :-
+    % write(Subset),nl,
+    member(X, List),
+    memberhelpertwo(Subset, List).
+
+% one
 memberhelper([], _).
 
 memberhelper([X|Subset], List) :-
-  member(X, List),
-  member([],[[]]),
-  memberhelper(Subset, List).
+    % write(Subset),nl,
+    member(X, List),
+    memberhelper(Subset, List).
 
+% when first argument is [] and second also is []
 subsetofall(Subset, List) :-
     length(Subset, L1),
     % write(L1),nl,
@@ -15,6 +26,7 @@ subsetofall(Subset, List) :-
     not(L2 = 0),
     subsetofallhelper(Subset, List).
 
+% when first argument is [] but second is not []
 subsetofall(Subset, List) :-
     length(Subset, L1),
     % write(L1),nl,
@@ -22,10 +34,24 @@ subsetofall(Subset, List) :-
     length(List, L2),
     % write(L2),nl,
     not(L2 = 0),
-    subsetofallhelper(Subset, List).
+    subsetofallhelpertwo(Subset, List).
 
-subsetofallhelper(_, []) :- !.
+% one
+subsetofallhelper(_, []) :-
+    % write(Subset),nl,
+    !.
 
 subsetofallhelper(Subset, [X|List]) :-
+    % write(X),nl,
     memberhelper(Subset, X),
     subsetofallhelper(Subset, List).
+
+% two
+subsetofallhelpertwo(_, []) :-
+    % write(Subset),nl,
+    !.
+
+subsetofallhelpertwo(Subset, [X|List]) :-
+    % write(X),nl,
+    memberhelpertwo(Subset, X),
+    subsetofallhelpertwo(Subset, List).
